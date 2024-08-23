e29de12b00c8bbeb49f9baca4c1478e3cabb1c7c
Omkarkothapally <168182655+Omkarkothapally@users.noreply.github.com>
2024-08-23 19:34:17 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -47,7 +47,7 @@ const News=(props)=> {
         <h1 className='text-center' style={{margin:'35px 0px',marginTop:'90px'}}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
        {loading && <Spinner/>}
        <InfiniteScroll
-        dataLength={articles.length}
+        dataLength={articles ? articles.length : 0}
         next={fetchMoreData}
         hasMore={articles.length !==totalResults}
         loader={<Spinner/>}
